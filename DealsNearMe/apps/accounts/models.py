815bfcf324b2e2ad9e2cb04c5ccf0fcaf3446675
Bryan Marty <bxm156@case.edu>
2013-08-10 16:06:40 -0700

    mention signals

--- DealsNearMe/apps/accounts/models.py
+++ DealsNearMe/apps/accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.contrib.localflavor.us.models import USStateField
 
@@ -12,3 +13,14 @@ class UserProfile(models.Model):
     favorite_color = models.CharField(max_length=255)
     latitude = models.IntegerField()
     longitude = models.IntegerField()
+
+
+def user_post_save(sender, instance, created, **kwargs):
+    if created is True:
+        # We could create a user profile model here for our user
+        # But we don't know any user profile information :(
+        # We would have to override the save() in the User
+        # createion form to pass extra attributes along, but at
+        # that point we could just create the user there
+        print "Post User Save Signal, instance: %s, kwargs: %s" % (instance, kwargs,)
+post_save.connect(user_post_save, sender=User)
